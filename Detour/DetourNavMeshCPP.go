8a87ea98b03e1ac90c447ed69237238b8499eda9
fananchong <fananchong@126.com>
2018-05-20 11:50:31 +0800

    Update DetourNavMeshCPP.go

--- Detour/DetourNavMeshCPP.go
+++ Detour/DetourNavMeshCPP.go
@@ -1,5 +1,10 @@
 package detour
 
+import (
+	"bytes"
+	"encoding/binary"
+)
+
 func (this *DtNavMesh) constructor() {
 
 }
@@ -15,6 +20,12 @@ func (this *DtNavMesh) destructor() {
 	this.m_tiles = nil
 }
 
+/// @{
+/// @name Initialization and Tile Management
+
+/// Initializes the navigation mesh for tiled use.
+///  @param[in]	params		Initialization parameters.
+/// @return The status flags for the operation.
 func (this *DtNavMesh) Init(params *DtNavMeshParams) DtStatus {
 	this.m_params = *params
 	DtVcopy(this.m_orig[:], params.Orig[:])
@@ -54,5 +65,66 @@ func (this *DtNavMesh) Init(params *DtNavMeshParams) DtStatus {
 	if this.m_saltBits < 10 {
 		return DT_FAILURE | DT_INVALID_PARAM
 	}
+	return DT_SUCCESS
+}
+
+/// Initializes the navigation mesh for single tile use.
+///  @param[in]	data		Data of the new tile. (See: #dtCreateNavMeshData)
+///  @param[in]	dataSize	The data size of the new tile.
+///  @param[in]	flags		The tile flags. (See: #dtTileFlags)
+/// @return The status flags for the operation.
+///  @see dtCreateNavMeshData
+func (this *DtNavMesh) Init2(data []byte, dataSize int, flags DtTileFlags) DtStatus {
+	// Make sure the data is in right format.
+	reader := bytes.NewReader(data)
+	header := &DtMeshHeader{}
+	if err := binary.Read(reader, binary.LittleEndian, header); err != nil {
+		return DT_FAILURE | DT_INVALID_PARAM
+	}
+	if header.Magic != DT_NAVMESH_MAGIC {
+		return DT_FAILURE | DT_WRONG_MAGIC
+	}
+	if header.Version != DT_NAVMESH_VERSION {
+		return DT_FAILURE | DT_WRONG_VERSION
+	}
+	var params DtNavMeshParams
+	DtVcopy(params.Orig[:], header.Bmin[:])
+	params.TileWidth = header.Bmax[0] - header.Bmin[0]
+	params.TileHeight = header.Bmax[2] - header.Bmin[2]
+	params.MaxTiles = 1
+	params.MaxPolys = uint32(header.PolyCount)
+
+	status := this.Init(&params)
+	if DtStatusFailed(status) {
+		return status
+	}
+	return this.AddTile(data, dataSize, flags, 0, nil)
+}
+
+/// Adds a tile to the navigation mesh.
+///  @param[in]		data		Data for the new tile mesh. (See: #dtCreateNavMeshData)
+///  @param[in]		dataSize	Data size of the new tile mesh.
+///  @param[in]		flags		Tile flags. (See: #dtTileFlags)
+///  @param[in]		lastRef		The desired reference for the tile. (When reloading a tile.) [opt] [Default: 0]
+///  @param[out]	result		The tile reference. (If the tile was succesfully added.) [opt]
+/// @return The status flags for the operation.
+
+/// @par
+///
+/// The add operation will fail if the data is in the wrong format, the allocated tile
+/// space is full, or there is a tile already at the specified reference.
+///
+/// The lastRef parameter is used to restore a tile with the same tile
+/// reference it had previously used.  In this case the #dtPolyRef's for the
+/// tile will be restored to the same values they were before the tile was
+/// removed.
+///
+/// The nav mesh assumes exclusive access to the data passed and will make
+/// changes to the dynamic portion of the data. For that reason the data
+/// should not be reused in other nav meshes until the tile has been successfully
+/// removed from this nav mesh.
+///
+/// @see dtCreateNavMeshData, #removeTile
+func (this *DtNavMesh) AddTile(data []byte, dataSize int, flags DtTileFlags, lastRef DtTileRef, result *DtTileRef) DtStatus {
 	return DT_SUCCESS
 }
--- tests/DetourNavMesh_test.go
+++ tests/DetourNavMesh_test.go
@@ -1,6 +1,9 @@
 package tests
 
 import (
+	"bytes"
+	"encoding/binary"
+	"io/ioutil"
 	"testing"
 
 	"github.com/fananchong/recastnavigation-go/Detour"
@@ -45,4 +48,78 @@ func Test_dtNavMesh1(t *testing.T) {
 	detour.DtAssert(salt1 == salt2)
 	detour.DtAssert(it1 == it2)
 	detour.DtAssert(ip1 == ip2)
+}
+
+func Test_dtNavMesh2(t *testing.T) {
+	data, err := ioutil.ReadFile("./data.dtMeshHeader")
+	detour.DtAssert(err == nil)
+	navMesh := detour.DtAllocNavMesh()
+	state := navMesh.Init2(data, len(data), 0)
+	detour.DtAssert(detour.DtStatusSucceed(state))
+}
+
+func Test_dtMeshHeader(t *testing.T) {
+	/* data.dtMeshHeader
+	   dtMeshHeader header;
+	   header.magic = DT_NAVMESH_MAGIC;
+	   header.version = DT_NAVMESH_VERSION;
+	   header.x = 300;
+	   header.y = 4000;
+	   header.layer = 50000;
+	   header.userId = 2;
+	   header.polyCount = 3;
+	   header.vertCount = 10002;
+	   header.maxLinkCount = 9999;
+	   header.detailMeshCount = 8888;
+	   header.detailVertCount = 9879;
+	   header.detailTriCount = 10923;
+	   header.bvNodeCount = 9083;
+	   header.offMeshConCount = 908;
+	   header.offMeshBase = 102;
+	   header.walkableHeight = 90;
+	   header.walkableRadius = 765;
+	   header.walkableClimb = 9;
+	   header.bmin[0] = 1.5f;
+	   header.bmin[1] = 2.5f;
+	   header.bmin[2] = 3.5f;
+	   header.bmax[0] = 2.6f;
+	   header.bmax[1] = 3.6f;
+	   header.bmax[2] = 4.6f;
+	   header.bvQuantFactor = 998;
+	   auto f = fopen("./data.dtMeshHeader", "wb");
+	   fwrite(&header, sizeof(dtMeshHeader), 1, f);
+	   fclose(f);
+	*/
+
+	data, err := ioutil.ReadFile("./data.dtMeshHeader")
+	detour.DtAssert(err == nil)
+	reader := bytes.NewReader(data)
+	header := &detour.DtMeshHeader{}
+	err = binary.Read(reader, binary.LittleEndian, header)
+	detour.DtAssert(err == nil)
+	detour.DtAssert(header.Magic == detour.DT_NAVMESH_MAGIC)
+	detour.DtAssert(header.Version == detour.DT_NAVMESH_VERSION)
+	detour.DtAssert(header.X == 300)
+	detour.DtAssert(header.Y == 4000)
+	detour.DtAssert(header.Layer == 50000)
+	detour.DtAssert(header.UserId == 2)
+	detour.DtAssert(header.PolyCount == 3)
+	detour.DtAssert(header.VertCount == 10002)
+	detour.DtAssert(header.MaxLinkCount == 9999)
+	detour.DtAssert(header.DetailMeshCount == 8888)
+	detour.DtAssert(header.DetailVertCount == 9879)
+	detour.DtAssert(header.DetailTriCount == 10923)
+	detour.DtAssert(header.BvNodeCount == 9083)
+	detour.DtAssert(header.OffMeshConCount == 908)
+	detour.DtAssert(header.OffMeshBase == 102)
+	detour.DtAssert(header.WalkableHeight == 90)
+	detour.DtAssert(header.WalkableRadius == 765)
+	detour.DtAssert(header.WalkableClimb == 9)
+	detour.DtAssert(IsEquals(header.Bmin[0], 1.5))
+	detour.DtAssert(IsEquals(header.Bmin[1], 2.5))
+	detour.DtAssert(IsEquals(header.Bmin[2], 3.5))
+	detour.DtAssert(IsEquals(header.Bmax[0], 2.6))
+	detour.DtAssert(IsEquals(header.Bmax[1], 3.6))
+	detour.DtAssert(IsEquals(header.Bmax[2], 4.6))
+	detour.DtAssert(IsEquals(header.BvQuantFactor, 998))
 }
